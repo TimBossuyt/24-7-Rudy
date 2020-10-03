513139ef514a243a55b38c047b6ae8153e16ed1e
Tim Bossuyt <tim.bossuyt@outlook.com>
2020-10-03 22:20:37 +0200

    activity change

--- rudy.py
+++ rudy.py
@@ -25,5 +25,6 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+    await client.change_presence(activity=discord.Game(name='Vroeger was alles beter'))
 
 client.run(os.environ['token'])
