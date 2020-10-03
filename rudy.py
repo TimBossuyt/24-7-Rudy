7891f9ab7d2c3bf23e23a8cc826c36d192175bd7
Tim Bossuyt <tim.bossuyt@outlook.com>
2020-10-03 22:14:22 +0200

    first commit

---
+++ rudy.py
@@ -0,0 +1,29 @@
+import discord
+import requests
+import os
+from googletrans import Translator, constants
+translator = Translator()
+
+def get_fact():
+    fact = requests.get('https://uselessfacts.jsph.pl/random.json').json()['text']
+    translation = translator.translate(fact, dest='nl')
+    return translation.text
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    print(message.content)
+    if message.content == '!weetje':
+        msg = get_fact()
+        await message.channel.send(msg)
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(os.environ['token'])
